7e6ca6f40c867417ba1a737ac105d384635ac6f2
cucuridas@gamil.com <3310223@naver.com>
2023-10-16 21:02:38 +0900

    서비스 실행을 위한 config 파일 추가

---
+++ app/core/config.py
@@ -0,0 +1,22 @@
+from dotenv import load_dotenv
+import os
+from typing import Any, Dict, Optional
+from pydantic import PostgresDsn, validator
+
+load_dotenv()
+
+def get_url():
+    user = os.getenv("POSTGRES_USER", "postgres")
+    password = os.getenv("POSTGRES_PASSWORD", "postgres")
+    server = os.getenv("POSTGRES_SERVER", "db")
+    db = os.getenv("POSTGRES_DB", "app")
+    return f"postgresql://{user}:{password}@{server}/{db}"
+class Settings():
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
+    SECRET_KEY= os.getenv("SECRET_KEY")
+    FIRST_SUPERUSER = os.getenv("FIRST_SUPERUSER")
+    FIRST_SUPERUSER_PASSWORD = os.getenv("FIRST_SUPERUSER_PASSWORD")
+    SQLALCHEMY_DATABASE_URI = get_url()
+
+
+settings = Settings()
