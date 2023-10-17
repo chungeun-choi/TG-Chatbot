ae2fab923ad23bf01d108a77394aa9a5cf388b4e
cucuridas@gamil.com <3310223@naver.com>
2023-10-17 20:19:10 +0900

    feat - team CRUD 서비스 추가 및 모델, 스키마 추가

--- app/api/v1/api.py
+++ app/api/v1/api.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
 
-from app.api.v1.endpoints import  login, user
+from app.api.v1.endpoints import  login, user, team
 
 api_router = APIRouter()
 api_router.include_router(login.router, tags=["login"])
 api_router.include_router(user.router, prefix="/users", tags=["users"])
+api_router.include_router(team.router, prefix="/teams", tags=["teams"])
