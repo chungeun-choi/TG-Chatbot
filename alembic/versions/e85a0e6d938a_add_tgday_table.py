612ebc131f025c63827ee219e21dbabe7fb9b4e4
cucuridas@gamil.com <3310223@naver.com>
2023-10-17 21:06:01 +0900

    feat - tgday 테이블 생성을 위한 alembic 파일 생성 및 정의

---
+++ alembic/versions/e85a0e6d938a_add_tgday_table.py
@@ -0,0 +1,35 @@
+"""Add tgday table
+
+Revision ID: e85a0e6d938a
+Revises: 9f7ea91d2267
+Create Date: 2023-10-17 15:45:10.751615
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'e85a0e6d938a'
+down_revision: Union[str, None] = '9f7ea91d2267'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        "tgday",
+        sa.Column("id", sa.Integer(), nullable=False, autoincrement=True),
+        sa.Column("user_id", sa.Integer(), nullable=False,),
+        sa.Column("regist_day",sa.TIMESTAMP(timezone=True), nullable=True),
+        sa.Column("created_at", sa.TIMESTAMP(timezone=True), server_default=sa.text("now()"), nullable=False),
+        sa.Column("updated_at", sa.TIMESTAMP(timezone=True), server_default=sa.text("now()"), nullable=False),
+        sa.PrimaryKeyConstraint("id"),
+    )
+
+    op.create_foreign_key("fk_tgday",source_table="tgday",referent_table="user",local_cols=["user_id"],remote_cols=["id"],ondelete="CASCADE")
+
+def downgrade() -> None:
+    pass
